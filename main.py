1d46b4a57fd6bbe6cea4ea6541685f1c4111ca00
Samg381 <Samg381@yahoo.com>
2021-01-15 17:13:21 -0500

    Added support for multiple items, enhanced timing cabability, and support for 'Coming soon' items.

--- main.py
+++ main.py
@@ -11,58 +11,65 @@ webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C://Program Fi
                                                                  "//Application//chrome.exe"))
 # Best Buy Bot
 # User defined variables
-DelayR = 8    # Specify delay (seconds) between stock checks. Beware too low a number.
-DelayP = 3    # Specify panic delay, or a lower delay to use during panic hours
+Panic = True   # Enable panic mode: increase the rate of stock checks between certain hours.
+DelayR = 8     # Specify delay (seconds) between stock checks. Beware too low a number.
+DelayP = 3     # Specify panic delay, or a lower delay to use during panic hours
 pHourS = 1045  # Panic hour start time (24hr format!)
 pHourE = 1100  # Panic hour end time
 
 Diag = True  # Enables / Disables alert dialogue boxes
 
-URL = 'https://www.bestbuy.com/site/evga-geforce-rtx-3080-ftw3-ultra-gaming-10gb-gddr6' \
-      '-pci-express-4-0-graphics-card/6436196.p?skuId=6436196'
+URLs = ['https://www.bestbuy.com/site/evga-geforce-rtx-3080-ftw3-ultra-gaming-10gb-gddr6-pci-express-4-0-graphics-card/6436196.p?skuId=6436196',
+        'https://www.bestbuy.com/site/evga-geforce-rtx-3080-ftw3-gaming-10gb-gddr6x-pci-express-4-0-graphics-card/6436191.p?skuId=6436191',
+        'https://www.bestbuy.com/site/evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6-pci-express-4-0-graphics-card/6432400.p?skuId=6432400'
+        ]
 
 # Declare our user agent. This tells the web server that we're a real browser, not a bot.
 headers = {'User-Agent': 'Chrome/75.0.3770.80'}
 
 
 def curtime():
+    adjustedhours = 0
+    adjustedmins = 0
     t1 = time.localtime()
     if len(str(t1.tm_min)) < 2:
-        t1.tm_min = str(0) + str(t1.tm_min)
+        adjustedmins = str(0) + str(t1.tm_min)
     if len(str(t1.tm_hour)) < 2:
-        t1.tm_hour = str(0) + str(t1.tm_hour)
-    return str(t1.tm_hour) + str(t1.tm_min)
+        adjustedhours = str(0) + str(t1.tm_hour)
+    return str(adjustedhours) + str(adjustedmins)
 
 
 while True:
-    print('The time is: ' + curtime())
+    # print('The time is: ' + curtime())
     if pHourS < int(curtime()) < pHourE:
         delay = DelayP
     else:
         delay = DelayR
 
-    # Load the webpage using our fake browser bot and save the data to the 'page' variable
-    page = requests.get(URL, headers=headers)
-    # Parse the content of the webpage in HTML format
-    soup = BeautifulSoup(page.content, 'lxml')
-    Focus = soup.find_all("div", class_=re.compile("fulfillment-add-to-cart-button"))
-    # print(Focus[0])
+    for x in URLs:
+        # Load the webpage using our fake browser bot and save the data to the 'page' variable
+        page = requests.get(x, headers=headers)
+        # Parse the content of the webpage in HTML format
+        soup = BeautifulSoup(page.content, 'lxml')
+        Focus = soup.find_all("div", class_=re.compile("fulfillment-add-to-cart-button"))
+        # print(Focus[0])
 
-    AddToCart = Focus[0].find_all(text=re.compile('Add to Cart'))
-    SoldOut = Focus[0].find_all(text=re.compile('Sold Out'))
+        AddToCart = Focus[0].find_all(text=re.compile('Add to Cart'))
+        SoldOut = Focus[0].find_all(text=re.compile('Sold Out|Coming Soon'))
 
-    if SoldOut:
-        print('[MESG] Item is out of stock.')
-    elif AddToCart:
-        webbrowser.get('chrome').open(URL)
-        x = 0
-        while x < 4:
-            print('[ALRT] ITEM IS IN STOCK!')
-            winsound.Beep(370 + x * 200, 80)
-            x += 1
-        if Diag:
-            ctypes.windll.user32.MessageBoxW(0, "Item is in stock!", "Stock Alert!", 1)
-    else:
-        print('[WARN] Unexpected error.')
-
-    time.sleep(delay)
+        if SoldOut:
+            print('[MESG] Item ' + str(URLs.index(x)) + ' is out of stock.')
+        elif AddToCart:
+            webbrowser.get('chrome').open(x)
+            x = 0
+            while x < 4:
+                print('[ALRT] ITEM ' + str(URLs.index(x)) + ' IS IN STOCK!')
+                winsound.Beep(370 + x * 200, 80)
+                x += 1
+            if Diag:
+                ctypes.windll.user32.MessageBoxW(0, 'Item ' + str(URLs.index(x)) + ' is in stock!', 'Stock Alert!', 1)
+        else:
+            print('[WARN] Unexpected error.')
+            if not AddToCart and not SoldOut:
+                print('         [DEBUG] Item ' + str(URLs.index(x)) + ' is neither sold out or in stock! Likely script error.')
+    time.sleep(delay - len(URLs))
